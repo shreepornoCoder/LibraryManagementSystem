8ee1125a9f9f5a94441729878b8d8333ab048b53
shreepornoCoder <sontos1979@gmail.com>
2024-08-11 22:44:59 +0600

    changes added

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import dj_database_url
-
+import os
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -135,14 +135,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     BASE_DIR /'static'
-# ]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR /'static',
+]
 
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / "media"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
